db31dd97cc5d609ad14a8bf65b84ab8364ec5ac9
Patryk Janowski <215745@edu.p.lodz.pl>
2020-03-31 00:58:25 +0200

    testy_v2 fixen in search method, throw exception when seq is empty

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -21,7 +21,12 @@ public class BinarySearch {
      * @return obiekt rezultatu o polach: - found (true jezeli znaleziony) - position (jezeli znaleziony - pozycja w
      *         sekwencji, jezeli nie znaleziony -1)
      */
-    public SearchResult search(int key, int[] seq) {
+    public SearchResult search(int key, int[] seq) throws IllegalArgumentException {
+
+        if (seq.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
         int start = 0;
         int end = seq.length - 1;
         int center;
